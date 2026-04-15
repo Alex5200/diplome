02aab6d72d04404f3d71420d328295b4e889edde
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2026-04-15 06:13:25 +0300

    Add controllerspackage with MotorController
    Introduce MotorController and MotorMonitor for motor control
    monitoring exported via __all__

---
+++ mujoco_sim/controllers/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Controllers Package
+
+Контроллеры приложения:
+    - MotorController: Управление моторами ST3215
+    - MotorMonitor: Асинхронный мониторинг состояния моторов
+"""
+
+from controllers.motor_controller import MotorController
+from controllers.motor_monitor import MotorMonitor
+
+__all__ = [
+    "MotorController",
+    "MotorMonitor",
+]
